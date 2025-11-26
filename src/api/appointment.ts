da73f6a66989c20cf4b2a053435e5a192c94afcc
Oleksandr Cheban <cebanoleksandr@gmail.com>
2025-11-26 14:01:19 +0200

    Add appointment api

---
+++ src/api/appointment.ts
@@ -0,0 +1,27 @@
+import type { CreateAppointmentDTO, GetAllAppointmentsDTO, UpdateAppointmentDTO } from "../utils/types";
+import { httpPrivate } from "./index";
+
+export const getAllAppointments = async (getAllAppointmentsDTO: GetAllAppointmentsDTO) => {
+  const response = await httpPrivate.get('/appointments', { params: getAllAppointmentsDTO });
+  return response.data;
+}
+
+export const getAppointmentById = async (id: string) => {
+  const response = await httpPrivate.get(`/appointments/${id}`);
+  return response.data;
+}
+
+export const createAppointment = async (appointmentData: CreateAppointmentDTO) => {
+  const response = await httpPrivate.post('/appointments', appointmentData);
+  return response.data;
+}
+
+export const updateAppointment = async (id: string, appointmentData: UpdateAppointmentDTO) => {
+  const response = await httpPrivate.put(`/appointments/${id}`, appointmentData);
+  return response.data;
+}
+
+export const deleteAppointment = async (id: string) => {
+  const response = await httpPrivate.delete(`/appointments/${id}`);
+  return response.data;
+}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -13,3 +13,33 @@ export type UpdateAdmin = {
   specializations?: string[];
   contacts?: IContact;
 }
+
+export type GetAllAppointmentsDTO = { 
+  masterId?: string; 
+  clientId?: string; 
+  serviceId?: string; 
+  status?: string; 
+  startTime?: string 
+}
+
+export type CreateAppointmentDTO = {
+  clientId: string;
+  masterId: string;
+  serviceId: string;
+  startTime: string;
+  status: Status;
+  finalPrice: number;
+  notes: string;
+}
+
+export type UpdateAppointmentDTO = {
+  clientId?: string;
+  masterId?: string;
+  serviceId?: string;
+  startTime?: string;
+  status?: Status;
+  finalPrice?: number;
+  notes?: string;
+}
+
+export type Status = 'BOOKED' | 'CONFIRMED' | 'COMPLETED' | 'CANCELED';
